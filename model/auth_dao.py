780147e1257b5a7c68c3dc2d9534fc24bce7f674
sangmin <sangminlee7648@gmail.com>
2021-08-01 17:44:34 +0900

    Feat: 추가 로그인 기능

--- model/auth_dao.py
+++ model/auth_dao.py
@@ -5,13 +5,62 @@ class AuthDao:
     def __init__(self, database):
         self.db = database
 
+    # 회원가입 시
     def insert_user(self, new_user):
         return self.db.execute(
             text(
                 """
-                INSERT INTO USERS (email, username, hashed_password)
-                VALUES (:email, :username, :hashed_password)
+                INSERT INTO USERS (email, username, gender, hashed_password)
+                VALUES (:email, :username, :gender, :password)
                 """
             ),
             new_user,
+        ).lastrowid
+
+    # 로그인 시 갖고 있는 해시 비번 같은지 확인, 비밀번호 찾기 시 기존 비밀번호 확인
+    def get_password(self, email):
+        return self.db.execute(
+            text(
+                """
+            SELECT hashed_password
+            FROM USERS
+            WHERE email=:email
+            """
+            ),
+            {"email": email},
+        ).fetchone()
+
+    # 로그인 시 새로 발급된 시크릿 키 삽입
+    def insert_new_secret_key(self, id, secret_key):
+        return self.db.execute(
+            text(
+                """
+            UPDATE USERS SET secret_key = ":secret_key"
+            WHERE id=:id
+            """
+            ),
+            {"id": id, "secret_key": secret_key},
         )
+
+    def get_secret_key(self, id):
+        return self.db.execute(
+            text(
+                """
+            SELECT secret_key FROM USERS
+            WHERE id=:id
+            """
+            ),
+            {"id": id},
+        ).fetchone()
+
+    # 비밀번호 찾기 시 이메일 확인
+    def get_id(self, email):
+        return self.db.execute(
+            text(
+                """
+            SELECT id FROM USERS
+            WHERE email=:email
+            """
+            ),
+            {"email": email},
+        ).fetchone()
--- service/auth_service.py
+++ service/auth_service.py
@@ -1,4 +1,8 @@
+import string
+import secrets
+
 import bcrypt
+import jwt
 
 
 class AuthService:
@@ -7,9 +11,44 @@ class AuthService:
         self.config = config
 
     def create_new_user(self, new_user):
-        new_user["hashed_password"] = bcrypt.hashpw(
+        new_user["password"] = bcrypt.hashpw(
             new_user["password"].encode("UTF-8"), bcrypt.gensalt()
         )
-
         new_user_id = self.auth_dao.insert_user(new_user)
         return new_user_id
+
+    def login(self, user_info):
+        email = user_info["email"]
+        password = user_info["password"]
+        user_private = self.auth_dao.get_password(email)
+        authorized = user_private[0] and bcrypt.checkpw(
+            password.encode("UTF-8"), user_private[0].encode("UTF-8")
+        )
+        return authorized
+
+    def generate_access_token(self, id, secret_key):
+        payload = {"user_id": id}
+        token = jwt.encode(payload, secret_key, "HS256")
+        return token
+
+    def get_secret_key(self, id):
+        secret_key = self.auth_dao.get_secret_key(id)
+        return secret_key[0]
+
+    # 로그인 시 새로 발급된 시크릿 키 삽입
+    def insert_new_secret_key(self, id):
+        string_pool = string.ascii_letters + string.digits
+        while True:
+            secret_key = "".join(secrets.choice(string_pool) for _ in range(10))
+            if (
+                any(c.islower() for c in secret_key)
+                and any(c.isupper() for c in secret_key)
+                and sum(c.isdigit() for c in secret_key) >= 3
+            ):
+                break
+        return self.auth_dao.insert_new_secret_key(id, secret_key)
+
+    # 비밀번호 찾기 시 id 확인, 토큰 생성 시 필요한 id
+    def check_id(self, email):
+        get_id = self.auth_dao.get_id(email)
+        return get_id[0]
