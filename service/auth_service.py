cb81a909214f7f0e02f1b85f7d796405ef0cb432
sangmin <sangminlee7648@gmail.com>
2021-08-02 22:39:37 +0900

    Feat: 추가 리뷰 기능

--- service/auth_service.py
+++ service/auth_service.py
@@ -28,58 +28,54 @@ class AuthService:
         new_user["password"] = bcrypt.hashpw(
             new_user["password"].encode("UTF-8"), bcrypt.gensalt()
         )
-        new_user["password"].decode('utf-8')
+        new_user["password"].decode("utf-8")
         new_user_id = self.auth_dao.insert_user(new_user)
         return new_user_id
 
     def login(self, user_info):
-        email = user_info['email']
-        password = user_info['password']
+        email = user_info["email"]
+        password = user_info["password"]
         user_private = self.auth_dao.get_password(email)
-        print(user_private)
 
         authorized = user_private[0] and bcrypt.checkpw(
-            password.encode('UTF-8'), user_private[0].encode('UTF-8'))
-            
-        print(authorized)
+            password.encode("UTF-8"), user_private[0].encode("UTF-8")
+        )
+
         return authorized
 
     def generate_access_token(self, user_id, secret_key):
-        payload = {     
-            'user_id': user_id
-        }
-        token = jwt.encode(payload, secret_key, 'HS256') 
-        print(f'token={token}')
+        payload = {"user_id": user_id}
+        token = jwt.encode(payload, secret_key, "HS256")
         return token
 
     def get_secret_key(self, user_id):
-        secret_key =  self.auth_dao.get_secret_key(user_id)
-        print(secret_key)
+        secret_key = self.auth_dao.get_secret_key(user_id)
         return secret_key[0]
 
     # 로그인 시 새로 발급된 시크릿 키 삽입
     def insert_new_secret_key(self, user_id):
         string_pool = string.ascii_letters + string.digits
         while True:
-            secret_key = ''.join(secrets.choice(string_pool) for _ in range(10))
-            if (any(c.islower() for c in secret_key) 
-                and any(c.isupper() for c in secret_key) 
-                and sum(c.isdigit() for c in secret_key) >= 3):
+            secret_key = "".join(secrets.choice(string_pool) for _ in range(10))
+            if (
+                any(c.islower() for c in secret_key)
+                and any(c.isupper() for c in secret_key)
+                and sum(c.isdigit() for c in secret_key) >= 3
+            ):
                 break
-        print(secret_key)
         return self.auth_dao.insert_new_secret_key(user_id, secret_key)
 
     # 비밀번호 찾기 시 id 확인, 토큰 생성 시 필요한 id
     def check_id(self, email):
-        get_id =  self.auth_dao.get_id(email)
-        return get_id[0]     
+        get_id = self.auth_dao.get_id(email)
+        return get_id[0]
 
     def check_having_temp_password(self, email):
         temp_password = self.auth_dao.get_temp_password(email)
         if temp_password:
             return True
         else:
-            return False 
+            return False
 
     # 비밀번호 찾기 시 발급받은 임시 비번 삽입
     def insert_temp_password(self, email, temp_password):
@@ -88,17 +84,12 @@ class AuthService:
     # 발급된 임시 비번 확인
     def check_temp_password(self, email, user_temp_password):
         temp_password = self.auth_dao.get_temp_password(email)
-        return user_temp_password == temp_password['temp_password']
+        return user_temp_password == temp_password["temp_password"]
 
     def update_temp_password(self, email, temp_password):
         return self.auth_dao.update_temp_password(email, temp_password)
 
     # 새로운 비번 삽입
     def insert_new_password(self, email, new_password):
-        hashed_password = bcrypt.hashpw(
-            new_password.encode("UTF-8"), bcrypt.gensalt()
-        )
+        hashed_password = bcrypt.hashpw(new_password.encode("UTF-8"), bcrypt.gensalt())
         return self.auth_dao.insert_new_password(email, hashed_password)
-
-
-
---
+++ service/reviews_service.py
@@ -0,0 +1,89 @@
+import os
+from datetime import date
+from flask import current_app
+
+
+class ReviewsService:
+    def __init__(self, reviews_dao):
+        self.reviews_dao = reviews_dao
+
+    def get_review_id(self, user_id, supplement_id):
+        review_id = self.reviews_dao.get_review_id(user_id, supplement_id)
+        return review_id
+
+    def get_reviews(self, user_id, supplement_id):
+        # 페이지네이션 필요
+        if user_id is not None and supplement_id is not None:
+            review = self.reviews_dao.get_review(user_id, supplement_id)
+
+            if review is None:
+                return None
+
+            review = review._asdict()
+            review_imgs = self.reviews_dao.get_review_imgs(review["id"])
+
+            new_review = {}
+            new_review_imgs = []
+            for review_img in review_imgs:
+                new_review_imgs.append(review_img._asdict()["img_url"])
+
+            new_review["imgUrls"] = new_review_imgs
+            new_review["reviewId"] = review["id"]
+            new_review["supplementId"] = review["supplement_id"]
+            new_review["userId"] = review["user_id"]
+            new_review["rating"] = review["rating"]
+            new_review["text"] = review["text"]
+            new_review["registrationDay"] = review["registration_day"]
+            return new_review
+
+        elif user_id is not None:
+            reviews = self.reviews_dao.get_reviews_by_user_id(user_id)
+        elif supplement_id is not None:
+            reviews = self.reviews_dao.get_reviews_by_supplement_id(supplement_id)
+
+        new_reviews = []
+        for review in reviews:
+            new_review = {}
+            new_review["supplementId"] = review[0]
+            new_review["supplementName"] = review[1]
+            new_review["companyName"] = review[2]
+            new_review["ratings"] = review[3]
+            new_reviews.append(new_review)
+        return new_reviews
+
+    def create_review(self, new_review):
+        new_review["registration_day"] = date.today().strftime("%Y-%m-%d")
+        review_id = self.reviews_dao.insert_review(new_review)
+        supplement_id = new_review["supplement_id"]
+        avg_rating, rating_count = self.reviews_dao.get_rating(supplement_id)
+        new_avg_rating = ((avg_rating * rating_count) + int(new_review["rating"])) / (
+            rating_count + 1
+        )
+        self.reviews_dao.increment_rating_count(supplement_id)
+        self.reviews_dao.update_avg_rating(supplement_id, new_avg_rating)
+        return review_id
+
+    def upload_review_imgs(self, review_id, review_imgs):
+        for img in review_imgs:
+            img_path = os.path.join(current_app.config["MEDIA_URL"], img.filename)
+            img.save(img_path)
+            self.reviews_dao.insert_img_url(review_id, img_path)
+        return None
+
+    def update_review(self, new_review):
+        self.reviews_dao.update_review(new_review)
+        supplement_id = new_review["supplement_id"]
+        avg_rating, rating_count = self.reviews_dao.get_rating(supplement_id)
+        new_avg_rating = ((avg_rating * rating_count) + int(new_review["rating"])) / (
+            rating_count + 1
+        )
+        self.reviews_dao.increment_rating_count(supplement_id)
+        self.reviews_dao.update_avg_rating(supplement_id, new_avg_rating)
+        return None
+
+    def delete_review(self, user_id, supplement_id):
+        review = self.get_review_id(user_id, supplement_id)
+        review_id = review[0]
+        imgs = self.reviews_dao.get_review_imgs(review_id)
+        self.reviews_dao.delete_review(user_id, supplement_id)
+        return None
